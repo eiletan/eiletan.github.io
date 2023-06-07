b56dce8ecc5d5abe8ceefebc05ada6447b6668ca
eiletan <nathanfhlai@gmail.com>
2023-06-07 15:34:36 -0700

    working on the project section of the website

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import './css/App.css';
 import Card from './components/Card';
 
 const websiteData = require('./assets/data.json');
-let introTitle= websiteData["introSection"]["title"];
-let introContent = websiteData["introSection"]["content"];
+let introSection= websiteData["introSection"];
 let projectTitle = websiteData["projectSection"]["title"];
 
 function App() {
   
+  // Takes content and creates a JSX expression for the component
+  function transformContentIntoJSX(data) {
+    if (typeof data === "string") {
+      let jsx = <div className="cardflexitem">
+          <p className="text">{data}</p>
+      </div>
+      return jsx;
+    }
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <Card classname="intro" title={introTitle} content={<div className="cardflexitem"><p className="text">{introContent}</p></div>}></Card>
+        <Card classname="intro" title={introSection["title"]} content={<div className="cardflexitem"><p className="text">{transformContentIntoJSX(introSection["content"])}</p></div>}></Card>
         <Card classname="project" title={projectTitle} ></Card>
       </header>
     </div>
