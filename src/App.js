58e28c26687201e6b4d45a16e898eeab362ae172
eiletan <nathanfhlai@gmail.com>
2023-06-10 15:42:52 -0700

    Building out project section of website

--- src/App.js
+++ src/App.js
@@ -9,23 +9,31 @@ let projectSection = websiteData["projectSection"];
 let hobbiesSection = websiteData["hobbiesSection"];
 
 function App() {
-  
-  // Takes content and creates a JSX expression for the component
-  function transformContentIntoJSX(data) {
-    if (typeof data === "string") {
-      let jsx = <div className="cardflexitem">
-          <p className="text">{data}</p>
-      </div>
-      return jsx;
-    }
+
+  function textAsContent(data) {
+    let jsx = <div className="cardflexitem">
+      <p className="text">{data}</p>
+    </div>
+    return jsx;
+  }
+
+  function previewCardAsContent(data) {
+    let cards = data.map((d,i) => {
+      let img = <img className="previewCardImage" src={d["image"]}></img>;
+      let content = <p className="text">{d["content"]}</p>;
+      let container = <div className="generalFlexContainer">{img}{content}</div>
+      return <Card cname="contentCardContainer" title={d["title"]} content={container} key={i} id={i}></Card>
+    })
+    return cards;
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <Card cname="intro" title={introSection["title"]} content={<div className="cardflexitem"><p className="text">{transformContentIntoJSX(introSection["content"])}</p></div>}></Card>
-        <Card cname="project" title={projectSection["title"]} ></Card>
-        <Card cname="hobbies" title={hobbiesSection["title"]} content={<div className="cardflexitem"><p className="text">{transformContentIntoJSX(hobbiesSection["content"])}</p></div>}></Card>
+        <Card cname="intro" title={introSection["title"]} content={textAsContent(introSection["content"])}></Card>
+        <Card cname="project" title={projectSection["title"]} content={<div className="generalFlexContainer">{previewCardAsContent(projectSection["content"])}</div>}></Card>
+        <Card cname="hobbies" title={hobbiesSection["title"]} content={textAsContent(hobbiesSection["content"])}></Card>
+        
       </header>
     </div>
   );
